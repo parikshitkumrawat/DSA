7aca056f6082df1bcb6f6714ec92a194f1943c9b
parikshitkumrawat <parikshitkumrawat@gmail.com>
2026-02-19 15:50:59 +0530

    Notes

---
+++ leetcode/53.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentsum=0;
+        int maxsum=nums[0];
+        for(int i=0;i<nums.size();i++){
+            currentsum+=nums[i];
+            if(currentsum>maxsum){
+                maxsum=currentsum;
+            }
+            if(currentsum<0){
+                currentsum=0;
+            }
+        }
+        return maxsum;
+    }
+};
