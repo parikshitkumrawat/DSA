1ad50bc4763293c272a0922bf95aef2330ade16e
parikshitkumrawat <parikshitkumrawat@gmail.com>
2026-02-19 15:48:36 +0530

    Notes

---
+++ lecture_24/1_bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //bubble sort
+    for(int i=0;i<n-i;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        cout<<"sorted array through bubble sort"<<endl;
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        return 0;
+    }
+}
